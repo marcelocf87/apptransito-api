1b33f14feab7ee433625330c5cd1a0374cbd890f
Marcelo <marceloo.cfreitas@gmail.com>
2024-03-01 16:29:49 -0400

    Implementação endpoint de inclusão de recurso

--- apptransito-api/src/main/java/com/marcelocf87/apptransito/api/controller/ProprietarioController.java
+++ apptransito-api/src/main/java/com/marcelocf87/apptransito/api/controller/ProprietarioController.java
@@ -3,11 +3,9 @@ package com.marcelocf87.apptransito.api.controller;
 import com.marcelocf87.apptransito.domain.model.Proprietario;
 import com.marcelocf87.apptransito.domain.repository.ProprietarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -30,6 +28,13 @@ public class ProprietarioController {
         return proprietarioRepository.findById(proprietarioId)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Proprietario adicionar(@RequestBody Proprietario proprietario) {
+        return proprietarioRepository.save(proprietario);
+
     }
 
 }
