2334c3b65400fb464ed7f506b754b92385669397
Marcelo <marceloo.cfreitas@gmail.com>
2024-03-01 16:14:12 -0400

    Refatorando o código e implementando endpoint de busca de recurso

--- apptransito-api/src/main/java/com/marcelocf87/apptransito/api/controller/ProprietarioController.java
+++ apptransito-api/src/main/java/com/marcelocf87/apptransito/api/controller/ProprietarioController.java
@@ -3,46 +3,33 @@ package com.marcelocf87.apptransito.api.controller;
 import com.marcelocf87.apptransito.domain.model.Proprietario;
 import com.marcelocf87.apptransito.domain.repository.ProprietarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/proprietarios")
 public class ProprietarioController {
 
    @Autowired
    private ProprietarioRepository proprietarioRepository;
 
-    @GetMapping("/proprietarios")
+    @GetMapping
     public List<Proprietario> listar() {
 //        return proprietarioRepository.findByNomeContaining("Maria");
         return proprietarioRepository.findAll();
-
-
-
-
-
-//        TypedQuery<Proprietario> query = manager.createQuery("from Proprietario", Proprietario.class);
-//        return query.getResultList();
-
-
-
-
-//
-//        var proprietario1 = new Proprietario();
-//        proprietario1.setId(1L);
-//        proprietario1.setNome("João Batista");
-//        proprietario1.setTelefone("99 99999-9999");
-//        proprietario1.setEmail("proprietario1@gmail.com");
-//
-//        var proprietario2 = new Proprietario();
-//        proprietario2.setId(2L);
-//        proprietario2.setNome("Maria de Fátima");
-//        proprietario2.setTelefone("99 99999-9999");
-//        proprietario2.setEmail("proprietario2@gmail.com");
-//
-//        return Arrays.asList(proprietario1, proprietario2);
-
     }
+
+    @GetMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> buscar(@PathVariable Long proprietarioId) {
+        return proprietarioRepository.findById(proprietarioId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 }
