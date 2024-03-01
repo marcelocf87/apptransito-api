d1d4f7fbcd79590f36dd8b936481341d9e6c4974
Marcelo <marceloo.cfreitas@gmail.com>
2024-03-01 15:30:38 -0400

    Injetando e usando o repositório do SDJ

--- apptransito-api/src/main/java/com/marcelocf87/apptransito/api/controller/ProprietarioController.java
+++ apptransito-api/src/main/java/com/marcelocf87/apptransito/api/controller/ProprietarioController.java
@@ -1,9 +1,11 @@
 package com.marcelocf87.apptransito.api.controller;
 
 import com.marcelocf87.apptransito.domain.model.Proprietario;
+import com.marcelocf87.apptransito.domain.repository.ProprietarioRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +15,15 @@ import java.util.List;
 @RestController
 public class ProprietarioController {
 
-    @PersistenceContext
-    private EntityManager manager;
+   @Autowired
+   private ProprietarioRepository proprietarioRepository;
 
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
-        return manager.createQuery("from Proprietario", Proprietario.class).getResultList();
+        return proprietarioRepository.findAll();
+
+
+
 
 
 //        TypedQuery<Proprietario> query = manager.createQuery("from Proprietario", Proprietario.class);
