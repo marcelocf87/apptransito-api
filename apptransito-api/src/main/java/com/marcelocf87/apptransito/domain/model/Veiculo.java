e7cb693cf5aebaa13a8c6bb195063d7f5cb057fd
Marcelo <marceloo.cfreitas@gmail.com>
2024-03-05 18:28:01 -0400

    Protegendo propriedades somente-leitura

--- apptransito-api/src/main/java/com/marcelocf87/apptransito/domain/model/Veiculo.java
+++ apptransito-api/src/main/java/com/marcelocf87/apptransito/domain/model/Veiculo.java
@@ -1,5 +1,6 @@
 package com.marcelocf87.apptransito.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -20,15 +21,19 @@ public class Veiculo {
 
     @ManyToOne
     private Proprietario proprietario;
-    private String marca;
 
+    private String marca;
     private String modelo;
     private String placa;
 
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @Enumerated(EnumType.STRING)
     private StatusVeiculo status;
 
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime dataCadastro;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime dataApreensao;
 
 
