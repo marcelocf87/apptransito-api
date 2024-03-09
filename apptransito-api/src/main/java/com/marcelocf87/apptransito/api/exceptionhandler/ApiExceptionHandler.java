176385ec9af4fce57d0cc8e19e8a3faec31248c6
Marcelo <marceloo.cfreitas@gmail.com>
2024-03-09 19:02:38 -0400

    Customizando as informações do ProblemDetail

--- apptransito-api/src/main/java/com/marcelocf87/apptransito/api/exceptionhandler/ApiExceptionHandler.java
+++ apptransito-api/src/main/java/com/marcelocf87/apptransito/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,17 +1,36 @@
 package com.marcelocf87.apptransito.api.exceptionhandler;
 
 import com.marcelocf87.apptransito.domain.exception.NegocioException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.net.URI;
 
 @RestControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatusCode status,
+                                                                  WebRequest request) {
+
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setTitle("Um ou mais campos estão inválidos");
+        problemDetail.setType(URI.create("https://apptransito.com/erros/campos-invalidos"));
+
+        return handleExceptionInternal(ex, problemDetail, headers, status, request);
+    }
+
     @ExceptionHandler(NegocioException.class)
     public ResponseEntity<String> capturar (NegocioException e) {
-        return super.handleMethodArgumentNotValid()ResponseEntity.badRequest().body(e.getMessage());
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
 
 }
