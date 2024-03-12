2af932d77273a4df1dd6f8df5c2b635197c7838d
Marcelo <marceloo.cfreitas@gmail.com>
2024-03-12 19:17:16 -0400

    Compondo objetos no Representation Model

---
+++ apptransito-api/src/main/java/com/marcelocf87/apptransito/api/model/ProprietarioResumoModel.java
@@ -0,0 +1,13 @@
+package com.marcelocf87.apptransito.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProprietarioResumoModel {
+
+    private Long id;
+    private String nome;
+
+}
