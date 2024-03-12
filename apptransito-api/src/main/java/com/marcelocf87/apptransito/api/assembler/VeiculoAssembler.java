4730a22673fa8084994ff641bc67a71fb1629c3d
Marcelo <marceloo.cfreitas@gmail.com>
2024-03-12 19:10:53 -0400

    Implementando assembler de Representation Model

---
+++ apptransito-api/src/main/java/com/marcelocf87/apptransito/api/assembler/VeiculoAssembler.java
@@ -0,0 +1,24 @@
+package com.marcelocf87.apptransito.api.assembler;
+
+import com.marcelocf87.apptransito.api.model.VeiculoModel;
+import com.marcelocf87.apptransito.domain.model.Veiculo;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Component
+public class VeiculoAssembler {
+
+    private final ModelMapper modelMapper;
+
+    public VeiculoModel toModel(Veiculo veiculo) {
+        return modelMapper.map(veiculo, VeiculoModel.class);
+    }
+
+    public List<VeiculoModel> toCollectionModel(List<Veiculo> veiculos) {
+        return veiculos.stream().map(this::toModel).toList();
+    }
+}
--- apptransito-api/src/main/java/com/marcelocf87/apptransito/api/controller/VeiculoController.java
+++ apptransito-api/src/main/java/com/marcelocf87/apptransito/api/controller/VeiculoController.java
@@ -1,12 +1,12 @@
 package com.marcelocf87.apptransito.api.controller;
 
+import com.marcelocf87.apptransito.api.assembler.VeiculoAssembler;
 import com.marcelocf87.apptransito.api.model.VeiculoModel;
 import com.marcelocf87.apptransito.domain.model.Veiculo;
 import com.marcelocf87.apptransito.domain.repository.VeiculoRepository;
 import com.marcelocf87.apptransito.domain.service.RegistroVeiculoService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,25 +20,25 @@ public class VeiculoController {
 
     private VeiculoRepository veiculoRepository;
     private final RegistroVeiculoService registroVeiculoService;
-    private final ModelMapper modelMapper;
+    private final VeiculoAssembler veiculoAssembler;
 
     @GetMapping
-    public List<Veiculo> listar(){
-        return veiculoRepository.findAll();
+    public List<VeiculoModel> listar(){
+        return veiculoAssembler.toCollectionModel(veiculoRepository.findAll());
     }
 
     @GetMapping("/{veiculoId}")
     public ResponseEntity<VeiculoModel> buscar(@PathVariable Long veiculoId) {
         return veiculoRepository.findById(veiculoId)
-                .map(veiculo -> modelMapper.map(veiculo, VeiculoModel.class))
+                .map(veiculoAssembler::toModel)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Veiculo cadastrar(@Valid @RequestBody Veiculo veiculo) {
-        return registroVeiculoService.cadastrar(veiculo);
+    public VeiculoModel cadastrar(@Valid @RequestBody Veiculo veiculo) {
+        return veiculoAssembler.toModel(registroVeiculoService.cadastrar(veiculo));
 
     }
 
