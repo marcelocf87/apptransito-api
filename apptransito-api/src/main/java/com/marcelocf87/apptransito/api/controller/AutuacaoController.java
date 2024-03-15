5d59404d37a6ce1c177d03691857fae56df11ab0
Marcelo <marceloo.cfreitas@gmail.com>
2024-03-15 19:48:53 -0400

    Implementando recurso de coleção de autuações

--- apptransito-api/src/main/java/com/marcelocf87/apptransito/api/controller/AutuacaoController.java
+++ apptransito-api/src/main/java/com/marcelocf87/apptransito/api/controller/AutuacaoController.java
@@ -4,11 +4,15 @@ import com.marcelocf87.apptransito.api.assembler.AutuacaoAssembler;
 import com.marcelocf87.apptransito.api.model.AutuacaoModel;
 import com.marcelocf87.apptransito.api.model.input.AutuacaoInput;
 import com.marcelocf87.apptransito.domain.model.Autuacao;
+import com.marcelocf87.apptransito.domain.model.Veiculo;
 import com.marcelocf87.apptransito.domain.service.RegistroAutuacaoService;
+import com.marcelocf87.apptransito.domain.service.RegistroVeiculoService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -17,6 +21,7 @@ public class AutuacaoController {
 
     private final AutuacaoAssembler autuacaoAssembler;
     private final RegistroAutuacaoService registroAutuacaoService;
+    private final RegistroVeiculoService registroVeiculoService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -26,6 +31,13 @@ public class AutuacaoController {
 
         return autuacaoAssembler.toModel(autuacaoRegistrada);
 
+    }
+
+    @GetMapping
+    public List<AutuacaoModel> listar(@PathVariable Long veiculoId) {
+        Veiculo veiculo = registroVeiculoService.buscar(veiculoId);
+
+        return autuacaoAssembler.toCollectionModel(veiculo.getAutuacoes());
     }
 
 }
